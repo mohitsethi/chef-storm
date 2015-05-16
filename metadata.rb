7159158e889db2dc9d78b292e0f905f6c0aad8a4
mohitsethi <mohit@sethis.in>
2015-05-16 22:13:07 +0530

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Mohit Sethi"
 maintainer_email "mohit@sethis.in"
 license          'MIT License'
 description      "Installs Twitter's Storm distributed computation system"
-version          "1.2.25"
+version          "1.2.26"
 depends          "java"
 depends          "runit"
 depends          "zookeeper"
